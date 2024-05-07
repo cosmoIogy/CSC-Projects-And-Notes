f98994153054a31d0db060b44a614797018ee82f
Blair Bourque <cosmomail12@gmail.com>
2024-05-07 11:04:00 -0500

    java started

--- Notes/05 Algorithms Reloaded/HelloWorld.java
+++ Notes/05 Algorithms Reloaded/HelloWorld.java
@@ -1,7 +1,15 @@
 class HelloWorld{
 
+    //Public means it is accessible outside of the class
+    //Private means it is only accessible inside the class
+    //Static means this function belongs to the class and not individual instances
+    //Void is the data type of what this function returns (it is not returning anything)
+    //Main is the name of the function
+    //String[] is the datatype for the parameter called args
+    //The brackets at the end of String say its an array of strings
     public static void main(String[] args){
-
+        //println is print line
+        //The double quotes are for strings
         System.out.println("Hello World!");
     }
 }
