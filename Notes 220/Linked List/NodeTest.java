2e6abfe33f653cecf968065d50c96ed62b184f90
Blair Bourque <cosmomail12@gmail.com>
2024-10-08 21:20:32 -0500

    new notes

--- Notes 220/Linked List/NodeTest.java
+++ Notes 220/Linked List/NodeTest.java
@@ -1,29 +1,53 @@
 class NodeTest {
     public static void main(String[] args) {
-        //create a node
-        Node n = new Node();
-        System.out.println(n);
-        n.setData(5);
-        System.out.println(n);
 
-        Node p = new Node();
-        p.setData(10);
+        int[] values = {1,2,3,4,5,6,7,8,9};
         
-        //set the link portion of the n node to p thereby linking them
-        p.setLink(p);
+        Node head = buildBackwards(values);
+    
+        }
 
-        //change the data portion of the second node
-        p.setData(15);
+        // curr = new Node();
 
-        p = new Node();
-        n.getLink().setLink(p);
+        // //create a node
+        // Node n = new Node();
+        // System.out.println(n);
+        // n.setData(5);
+        // System.out.println(n);
 
-        p.setLink(new Node());
-        p.getLink().setData(100);
+        // Node p = new Node();
+        // p.setData(10);
+        
+        // //set the link portion of the n node to p thereby linking them
+        // p.setLink(p);
+
+        // //change the data portion of the second node
+        // p.setData(15);
+
+        // p = new Node();
+        // n.getLink().setLink(p);
+
+        // p.setLink(new Node());
+        // p.getLink().setData(100);
 
         //print the linked list
-        printList(n);
+        printList(head)
+
+    public static Node buildBackwards(int[] array){
+        Node head = null, curr;
+        int count = array.length;
+
+        while(count > 0){
+            curr = new Node();
+            curr.setData(values[count - 1]);
+            curr.setLink(head);
+            head = curr;
+
+            count--;
     }
+    }
+
+
     public static void printList(Node temp){
         while (temp != null){
             System.out.print(temp);
